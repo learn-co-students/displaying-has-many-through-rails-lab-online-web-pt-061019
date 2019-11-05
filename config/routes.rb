647b1c435d1573978c26fd1920c45967b4538cfa
Khris Punzalan <khrispunzalan@gmail.com>
2019-11-04 19:53:23 -0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :doctors
   resources :patients
+  resources :doctors
+  # resources :appointments
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :appointments, only: [:show]
 end
