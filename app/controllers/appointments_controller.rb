1554b15e04bb374208113585444bb0fcaadc1496
alicia <afasciocco@gmail.com>
2019-11-07 21:24:24 -0500

    Done.

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,13 @@
+class AppointmentsController < ApplicationController
+
+    def show
+        @appointment = Appointment.find(params[:id])
+    end
+
+    private
+
+    def appointment_params
+        params.require(:appointment).permit(:appointment_datetime, :patient, :doctor)
+    end
+
+end
